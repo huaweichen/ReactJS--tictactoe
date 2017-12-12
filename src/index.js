a4890c53c69678daae513357a6f96ce297f3e90b
Huawei Chen <huawei.chen@xvt.com.au>
2017-12-12 13:40:29 +1000

    clean up and init.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(
+		<h1>Welcome to Tic-Tac-Toe game.</h1>,
+		document.getElementById('root')
+)

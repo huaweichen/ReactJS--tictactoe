4c084a72b589292b0ee6b51f61cdcb78274ebdea
Huawei Chen <huawei.chen@xvt.com.au>
2017-12-14 12:06:09 +1000

    1. build up profile page.

--- src/containers/Profile.js
+++ src/containers/Profile.js
@@ -1,12 +1,96 @@
 import React, {Component} from 'react'
+import {Container, Name, GameListHeader, GameList, GameRecord, Column, ColumnLabels} from "../styled/Profile"
 
 class Profile extends Component {
 
+	static defaultProps = {
+		user: {
+			email: 'USER_EMAIL',
+			games: [
+				{
+					winner: true,
+					createdAt: '12/12/2017',
+					id: '001'
+				},
+				{
+					winner: true,
+					createdAt: '12/12/2017',
+					id: '002'
+				},
+				{
+					winner: false,
+					createdAt: '12/12/2017',
+					id: '003'
+				},
+				{
+					winner: false,
+					createdAt: '12/12/2017',
+					id: '004'
+				},
+				{
+					winner: true,
+					createdAt: '12/12/2017',
+					id: '005'
+				}
+			]
+		}
+	}
+
+	get records() {
+		return this.props.user.games.map((game, index) => {
+			return (
+				<GameRecord
+					key={index}
+					index={game.id}
+				>
+					<Column>
+						{(game.winner) ? 'Won!' : 'Lost...'}
+					</Column>
+					<Column>
+						"AI Player"
+					</Column>
+					<Column>
+						"Correct!"
+					</Column>
+					<Column>
+						{game.createdAt}
+					</Column>
+				</GameRecord>
+			)
+		})
+	}
+
 	render() {
+		let {
+			email,
+			games
+		} = this.props.user
 		return (
-			<div>
-					<h2>Profile page here.</h2>
-			</div>
+				<Container>
+					<Name>
+						{email}
+					</Name>
+					<GameList>
+						<GameListHeader>
+							MyGames
+						</GameListHeader>
+						<ColumnLabels>
+							<Column>
+								Outcome
+							</Column>
+							<Column>
+								Guess
+							</Column>
+							<Column>
+								Guessed Correctly
+							</Column>
+							<Column>
+								Date
+							</Column>
+						</ColumnLabels>
+						{this.records}
+					</GameList>
+				</Container>
 		)
 	}
 
